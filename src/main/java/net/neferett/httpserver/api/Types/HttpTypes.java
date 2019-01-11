705ee03c5ac2a1f68ee4e7cd1a9237c73690ad8a
Neferett <30388371+jordanbonaldi@users.noreply.github.com>
2019-01-11 23:54:10 +0100

    Miss Class

--- src/main/java/net/neferett/httpserver/api/Types/HttpTypes.java
+++ src/main/java/net/neferett/httpserver/api/Types/HttpTypes.java
@@ -1,12 +1,13 @@
 package net.neferett.httpserver.api.Types;
 
 import lombok.Getter;
+import net.neferett.httpserver.api.Handler.HTTPSServerHandler;
 import net.neferett.httpserver.api.Handler.HTTPServerHandler;
 import net.neferett.httpserver.api.Handler.HttpServerInterface;
 
 public enum HttpTypes {
 
-    HTTP(HTTPServerHandler.class), HTTPS(HTTPServerHandler.class);
+    HTTP(HTTPServerHandler.class), HTTPS(HTTPSServerHandler.class);
 
     @Getter
     private final Class<? extends HttpServerInterface> type;
